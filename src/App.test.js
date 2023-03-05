becf22314e47cfedc3d15da7bc4a1e8050688922
plu457 <ekgus5095@gmail.com>
2023-03-05 16:57:30 +0900

    ADD: eslint 및 테스트 기본 설정

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   render(<App />);
   //* const { getByText } = render(<App />);
   //* 소스 코드가 많아지면 비추천
   const linkElement = screen.getByText(/Learn React/i);
   expect(linkElement).toBeInTheDocument();
+
+  const lintTest = screen.getByRole('button', {
+    name: 'lintTest',
+  });
+  expect(lintTest.textContent).toBe('lintTest');
 });
