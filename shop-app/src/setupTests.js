af4cf0f28b34f390f04b7cd3258530dbbeb6ddd3
plu457 <ekgus5095@gmail.com>
2023-03-19 19:41:17 +0900

    Docs: resetHanlders 함수를 사용하는 이유에 대한 내용 추가

--- shop-app/src/setupTests.js
+++ shop-app/src/setupTests.js
@@ -8,6 +8,8 @@ import { server } from "./mocks/server";
 
 beforeAll(() => server.listen());
 
+//* 각 테스트 케이스는 서로 영향을 미치지 않도록 독립적으로 실행되어야 한다.
+//* 만약 이전 테스트 케이스에서 사용된 mock 핸들러 함수들의 상태가 그대로 유지되어 있다면, 이전 테스트 케이스에서의 결과가 다음 테스트 케이스에 영향을 미치게 된다.
 afterEach(() => server.resetHandlers());
 
 afterAll(() => server.close());
